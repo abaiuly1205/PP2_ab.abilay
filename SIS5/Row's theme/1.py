daac1cf41b750d70bd738e55aeb086ee9686df0b
Abylai <abaiuly1205@gmail.com>
2024-02-25 02:27:29 +0600

    a

---
+++ SIS5/Row's theme/1.py
@@ -0,0 +1,21 @@
+import re
+import csv
+
+f = open("row.txt", "r",encoding="utf8")
+text = f.read()
+
+pattern = r'\n(?P<reti>[0-9]+)\.\n(?P<aty>.+)\n(?P<sany>[1-9]+\,.+)x(?P<bagasy>[1-9]+\,.+)\n(?P<quny>[1-9]+.+)'
+
+res = re.finditer(pattern, text)
+
+with open('data.csv', 'w', newline='',encoding="utf8") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['reti', 'aty', 'sany', 'bagasy', 'quny'])
+    for x in res:
+        writer.writerow([
+            x.group('reti'), 
+            x.group('aty'),
+            float(x.group('sany').strip().replace(',','.')),
+            float(x.group('bagasy').strip().replace(',','.').replace(' ','')),
+            float(x.group('quny').strip().replace(',','.').replace(' ',''))
+        ])
