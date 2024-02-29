3398ff30ce5737a3b772a96f60db204b4ae4921e
Abylai <abaiuly1205@gmail.com>
2024-03-01 04:17:30 +0500

    upd

--- SIS6/dir/3.py
+++ SIS6/dir/3.py
@@ -1,6 +1,6 @@
 import os
 
-path = '/Users/abylay/PP2_ab.abilay/SIS6'
+path = r'/Users/abylay/PP2_ab.abilay/SIS6/text.txt'
 if os.access(path, os.F_OK):
      print("\n" + os.path.basename(path))
      print("\n" + os.path.dirname(path) + "\n")
