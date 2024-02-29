ee52b40a485510beec7f0be7d9461e4dafda6788
Abylai <abaiuly1205@gmail.com>
2024-02-29 15:50:30 +0600

    upd

--- SIS6/dir/1.py
+++ SIS6/dir/1.py
@@ -5,4 +5,13 @@ path = os.getcwd()
 dir_list = os.listdir(path) 
    
 print("Files and directories in '", path, "' :")  
-print(dir_list)
+for x in dir_list: 
+        print(x)
+print("Directories:") 
+for dir in dir_list: 
+    if os.path.isdir(os.path.join(path, dir)):
+        print(dir)
+print("Files:")   
+for file in dir_list:
+    if os.path.isfile(os.path.join(path, file)):
+        print(file)
