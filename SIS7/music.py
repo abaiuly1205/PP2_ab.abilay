4d669b3bc1f6f50dbe39d73af7e93fed7e746de8
Abylai <abaiuly1205@gmail.com>
2024-03-15 07:07:41 +0500

    upd

--- SIS7/music.py
+++ SIS7/music.py
@@ -8,12 +8,18 @@ pygame.display.set_icon(pygame.image.load('images/music.png'))
 _songs = [
     'music/Дос-Мұқасан - 16 қыз.mp3',
     'music/Kilgore Doubtfire - Escape.mp3',
-    'music/The Weeknd - Out of Time.mp3'
+    'music/The Weeknd - Out of Time.mp3',
+    'music/XXXtentacion - Jocelyn Flores.mp3',
+    'music/Justin Bieber - Confident.mp3',
+    'music/Post Malone - Circles.mp3'
 ]
 covers = [
     pygame.image.load('images/0.jpg'),
     pygame.image.load('images/1.jpg'),
-    pygame.image.load('images/2.jpg')
+    pygame.image.load('images/2.jpg'),
+    pygame.image.load('images/3.jpg'),
+    pygame.image.load('images/4.jpg'),
+    pygame.image.load('images/5.jpg')
 ]
 done = True
 i = 0
@@ -37,13 +43,13 @@ while done:
             pygame.mixer.music.play()
         if pressed[pygame.K_LEFT]:
             pygame.mixer.music.unload()
-            i = 2
+            i = 5
             screen.blit(covers[i], (0, 0))
             pygame.mixer.music.load(_songs[i])
             pygame.mixer.music.play()
         if pressed[pygame.K_SPACE]: pygame.mixer.music.pause()
         if pressed[pygame.K_p]: pygame.mixer.music.unpause()
-    elif i > 0 and i < 2:
+    elif i > 0 and i < 5:
         if pressed[pygame.K_RIGHT]:
             pygame.mixer.music.unload()
             i = i + 1
@@ -58,7 +64,7 @@ while done:
             pygame.mixer.music.play()
         if pressed[pygame.K_SPACE]: pygame.mixer.music.pause()
         if pressed[pygame.K_p]: pygame.mixer.music.unpause()
-    elif i == 2:
+    elif i == 5:
         if pressed[pygame.K_RIGHT]: 
             pygame.mixer.music.unload()
             i = 0
