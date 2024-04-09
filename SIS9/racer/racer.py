918cc7de14f667198ddefcb4a8ab3dd955cb59ac
Abylai <abaiuly1205@gmail.com>
2024-04-09 23:51:18 +0500

    upd

--- SIS9/racer/racer.py
+++ SIS9/racer/racer.py
@@ -3,21 +3,16 @@ import pygame, sys
 from pygame.locals import *
 import random, time
 
-# Initialzing
 pygame.init()
-
-# Setting up FPS
 FPS = 60
 FramePerSec = pygame.time.Clock()
 
-# Creating colors
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-# Other Variables for use in the program
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 SPEED = 5
@@ -25,14 +20,10 @@ COIN_SPEED = 5
 SCORE = 0
 COIN = 0
 
-# Setting up Fonts
 font = pygame.font.SysFont("Verdana", 60)
 font_small = pygame.font.SysFont("Verdana", 20)
 loh = font.render("Game Over", True, BLACK)
-
 background = pygame.image.load("AnimatedStreet.png")
-
-# Create a white screen
 DISPLAYSURF = pygame.display.set_mode((400, 600))
 DISPLAYSURF.fill(WHITE)
 pygame.display.set_caption("Game")
@@ -53,8 +44,6 @@ class Enemy(pygame.sprite.Sprite):
             self.rect.top = 0
             self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
 
-
-# creating new class
 class Coin(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -71,7 +60,6 @@ class Coin(pygame.sprite.Sprite):
     def reset(self):
         self.weight = random.randint(1,5)
         self.rect.center = (random.randint(80, SCREEN_WIDTH - 80), 0)
-
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -90,13 +78,10 @@ class Player(pygame.sprite.Sprite):
             if pressed_keys[K_RIGHT]:
                 self.rect.move_ip(5, 0)
 
-
-# Setting up Sprites
 P1 = Player()
 E1 = Enemy()
 C1 = Coin()
 
-# Creating Sprites Groups
 enemies = pygame.sprite.Group()
 enemies.add(E1)
 
@@ -108,14 +93,10 @@ all_sprites.add(P1)
 all_sprites.add(E1)
 all_sprites.add(C1)
 
-# Adding a new User event
 INC_SPEED = pygame.USEREVENT + 1
 pygame.time.set_timer(INC_SPEED, 1000)
 
-# Game Loop
 while True:
-
-    # Cycles through all events occuring
     for event in pygame.event.get():
         if event.type == INC_SPEED and COIN % 40 == 0:
             SPEED += 0.5
@@ -124,30 +105,25 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # display on we see
     DISPLAYSURF.blit(background, (0, 0))
     scores = font_small.render(str(SCORE), True, BLACK)
     DISPLAYSURF.blit(scores, (15, 10))
     coins = font_small.render(str(COIN), True, BLACK)
     DISPLAYSURF.blit(coins, (370, 10))
 
-    # Moves and Re-draws all Sprites
     for entity in all_sprites:
         entity.move()
         DISPLAYSURF.blit(entity.image, entity.rect)
 
 
-#speed of coin
     if C1.weight > 3 and  COIN_SPEED<= 3:
         COIN_SPEED += 0.3
     elif C1.weight <=3 and COIN_SPEED > 6 :
         COIN_SPEED -= 0.4
-    # To be run if collision occurs between Player and bonus
     if pygame.sprite.spritecollideany(P1, bonuc):
         COIN += C1.weight
         C1.reset()
-
-    # To be run if collision occurs between Player and Enemy
+        
     if pygame.sprite.spritecollideany(P1, enemies):
         pygame.mixer.Sound('crash.wav').play()
         time.sleep(1)
